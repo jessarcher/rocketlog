77123094dd8cdc0033a0efa3c5b177579c3a426e
Jess Archer <jess@jessarcher.com>
2020-09-23 20:49:02 +1000

    Fix login link on mobile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,9 +21,9 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 items-center sm:pt-0">
             @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                <div class="fixed top-0 right-0 px-6 py-4">
                     @auth
                         <a href="{{ url('/daily-log') }}" class="text-sm text-gray-700 underline">Your Log</a>
                     @else
