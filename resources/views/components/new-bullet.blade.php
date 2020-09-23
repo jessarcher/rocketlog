831a3af3804655add18bb3948e3d833c27aac839
Jess Archer <jess@jessarcher.com>
2020-09-23 19:22:29 +1000

    Tweak bullet loader to match new bullet sizes

--- resources/views/components/new-bullet.blade.php
+++ resources/views/components/new-bullet.blade.php
@@ -6,7 +6,7 @@
     x-ref="form"
 >
     <div class="border border-transparent flex-shrink-0">
-        <div class="h-8 w-8 flex items-center justify-center">
+        <div class="h-10 w-10 md:h-8 md:w-8 flex items-center justify-center">
             <x-icon.incomplete class="h-5 w-5 text-gray-200" />
         </div>
     </div>
@@ -33,8 +33,8 @@
         <x-jet-input-error for="newBulletName" class="mt-2" />
     </div>
 
-    <div class="w-8 h-8 flex items-center justify-center">
-        <svg wire:loading wire:target="addBullet" class="animate-spin h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <div class="w-10 h-10 md:w-8 md:h-8 border border-transparent flex items-center justify-center">
+        <svg wire:loading wire:target="addBullet" class="animate-spin h-6 w-6 md:h-5 md:w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
             <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
         </svg>
--- resources/views/livewire/bullet.blade.php
+++ resources/views/livewire/bullet.blade.php
@@ -119,8 +119,8 @@
         <x-jet-input-error for="bullet.state" class="mt-2" />
     </div>
 
-    <div class="w-8 h-8 flex items-center justify-center">
-        <svg wire:loading class="animate-spin h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <div class="w-10 h-10 md:w-8 md:h-8 border border-transparent flex items-center justify-center">
+        <svg wire:loading class="animate-spin h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
             <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
         </svg>
