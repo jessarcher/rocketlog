2b95939d625af14471cb8211fac3b843bd0e06c5
Jess Archer <jess@jessarcher.com>
2020-09-23 18:21:17 +1000

    Make bullet buttons bigger on mobile

--- resources/views/livewire/bullet.blade.php
+++ resources/views/livewire/bullet.blade.php
@@ -4,43 +4,43 @@
         state: @entangle('bullet.state'),
         deleting: false,
     }"
-    class="py-2 border-b border-gray-200 flex"
+    class="py-1 md:py-2 border-b border-gray-200 flex"
     x-init="autosize($refs.name)"
 >
     <div class="relative flex-shrink-0" :class="deleting ? 'opacity-25' : ''">
         @if ($type === 'bullet')
             <div class="border border-transparent">
                 <button
-                    class="h-8 w-8 flex items-center justify-center rounded-full border border-transparent hover:border-gray-200 hover:shadow focus:outline-none focus:border-gray-200 focus:shadow-inner"
+                    class="h-10 w-10 md:h-8 md:w-8 flex items-center justify-center rounded-full border border-transparent hover:border-gray-200 hover:shadow focus:outline-none focus:border-gray-200 focus:shadow-inner"
                     :class="[
                         ! $wire.fade && (state === 'incomplete' || state === 'note' || state === 'event') ? 'text-gray-700' : 'text-gray-400',
                     ]"
                     @click="menu = true"
                 >
                     @if ($bullet->state === 'incomplete')
-                        <x-icon.incomplete x-show="state === ''" class="h-5 w-5 text-gray-400" />
+                        <x-icon.incomplete x-show="state === ''" class="h-6 w-6 md:h-5 md:h-5 text-gray-400" />
                     @endif
-                    <x-icon.incomplete x-cloak x-show="state === 'incomplete'" class="w-5 h-5" />
+                    <x-icon.incomplete x-cloak x-show="state === 'incomplete'" class="h-6 w-6 md:h-5 md:h-5" />
 
                     @if ($bullet->state === 'complete')
-                        <x-icon.complete x-show="state === ''" class="h-5 w-5 text-gray-400" />
+                        <x-icon.complete x-show="state === ''" class="h-6 w-6 md:h-5 md:h-5 text-gray-400" />
                     @endif
-                    <x-icon.complete x-cloak x-show="state === 'complete'" class="h-5 w-5" />
+                    <x-icon.complete x-cloak x-show="state === 'complete'" class="h-6 w-6 md:h-5 md:h-5" />
 
                     @if ($bullet->state === 'migrated')
-                        <x-icon.migrated x-show="state === ''" class="w-5 h-5 text-gray-400" />
+                        <x-icon.migrated x-show="state === ''" class="h-6 w-6 md:h-5 md:h-5 text-gray-400" />
                     @endif
-                    <x-icon.migrated x-cloak x-show="state === 'migrated'" class="w-5 h-5" />
+                    <x-icon.migrated x-cloak x-show="state === 'migrated'" class="h-6 w-6 md:h-5 md:h-5" />
 
                     @if ($bullet->state === 'note')
-                        <x-icon.note x-show="state === ''" class="w-5 h-5 text-gray-400" />
+                        <x-icon.note x-show="state === ''" class="h-6 w-6 md:h-5 md:h-5 text-gray-400" />
                     @endif
-                    <x-icon.note x-cloak x-show="state === 'note'" class="w-5 h-5" />
+                    <x-icon.note x-cloak x-show="state === 'note'" class="h-6 w-6 md:h-5 md:h-5" />
 
                     @if ($bullet->state === 'event')
-                        <x-icon.event x-show="state === ''" class="w-5 h-5 text-gray-400" />
+                        <x-icon.event x-show="state === ''" class="h-6 w-6 md:h-5 md:h-5 text-gray-400" />
                     @endif
-                    <x-icon.event x-cloak x-show="state === 'event'" class="w-5 h-5" />
+                    <x-icon.event x-cloak x-show="state === 'event'" class="h-6 w-6 md:h-5 md:h-5" />
                 </button>
             </div>
 
@@ -51,40 +51,40 @@
                 @click.away="menu = false"
                 @click="menu = false"
             >
-                <button class="h-8 w-8 flex items-center justify-center focus:outline-none" @click="menu = false">
-                    <x-icon.complete x-show="state === 'complete'" class="h-5 w-5" />
-                    <x-icon.incomplete x-show="state === 'incomplete'" class="w-5 h-5" />
-                    <x-icon.migrated x-show="state === 'migrated'" class="w-5 h-5" />
-                    <x-icon.note x-show="state === 'note'" class="w-5 h-5" />
-                    <x-icon.event x-show="state === 'event'" class="w-5 h-5" />
+                <button class="h-10 w-10 md:h-8 md:w-8 flex items-center justify-center focus:outline-none" @click="menu = false">
+                    <x-icon.complete x-show="state === 'complete'" class="h-6 w-6 md:h-5 md:h-5" />
+                    <x-icon.incomplete x-show="state === 'incomplete'" class="h-6 w-6 md:h-5 md:h-5" />
+                    <x-icon.migrated x-show="state === 'migrated'" class="h-6 w-6 md:h-5 md:h-5" />
+                    <x-icon.note x-show="state === 'note'" class="h-6 w-6 md:h-5 md:h-5" />
+                    <x-icon.event x-show="state === 'event'" class="h-6 w-6 md:h-5 md:h-5" />
                 </button>
 
-                <button x-show="state !== 'incomplete'" class="h-8 w-8 flex items-center justify-center hover:bg-gray-100 focus:outline-none" @click="state = 'incomplete'">
-                    <x-icon.incomplete class="w-5 h-5" />
+                <button x-show="state !== 'incomplete'" class="h-10 w-10 md:h-8 md:w-8 flex items-center justify-center hover:bg-gray-100 focus:outline-none" @click="state = 'incomplete'">
+                    <x-icon.incomplete class="w-6 h-6 md:w-5 md:h-5" />
                 </button>
 
-                <button x-show="state !== 'complete'" class="h-8 w-8 flex items-center justify-center hover:bg-gray-100 focus:outline-none" @click="state = 'complete'">
-                    <x-icon.complete class="h-5 w-5" />
+                <button x-show="state !== 'complete'" class="h-10 w-10 md:h-8 md:w-8 flex items-center justify-center hover:bg-gray-100 focus:outline-none" @click="state = 'complete'">
+                    <x-icon.complete class="w-6 h-6 md:w-5 md:h-5" />
                 </button>
 
-                <button x-show="state !== 'note'" class="h-8 w-8 flex items-center justify-center hover:bg-gray-100 focus:outline-none" @click="state = 'note'">
-                    <x-icon.note class="h-5 w-5" />
+                <button x-show="state !== 'note'" class="h-10 w-10 md:h-8 md:w-8 flex items-center justify-center hover:bg-gray-100 focus:outline-none" @click="state = 'note'">
+                    <x-icon.note class="w-6 h-6 md:w-5 md:h-5" />
                 </button>
 
                 @if ($bullet->collection_id === null)
-                    <button x-show="state !== 'event'" class="h-8 w-8 flex items-center justify-center hover:bg-gray-100 focus:outline-none" @click="state = 'event'">
-                        <x-icon.event class="h-5 w-5" />
+                    <button x-show="state !== 'event'" class="h-10 w-10 md:h-8 md:w-8 flex items-center justify-center hover:bg-gray-100 focus:outline-none" @click="state = 'event'">
+                        <x-icon.event class="w-6 h-6 md:w-5 md:h-5" />
                     </button>
                 @endif
 
                 @if ($bullet->collection_id === null && $bullet->created_at <= today())
-                    <button x-show="state !== 'migrated'" class="h-8 w-8 flex items-center justify-center hover:bg-gray-100 focus:outline-none" @click="state = 'migrated'" wire:click="migrate">
-                        <x-icon.migrated class="h-5 w-5" />
+                    <button x-show="state !== 'migrated'" class="h-10 w-10 md:h-8 md:w-8 flex items-center justify-center hover:bg-gray-100 focus:outline-none" @click="state = 'migrated'" wire:click="migrate">
+                        <x-icon.migrated class="w-6 h-6 md:w-5 md:h-5" />
                     </button>
                 @endif
 
-                <button class="h-8 w-8 flex items-center justify-center hover:bg-gray-100 focus:outline-none" @click="deleting = true" wire:click="delete">
-                    <x-heroicon-s-trash class="h-5 w-5 text-gray-500" />
+                <button class="h-10 w-10 md:h-8 md:w-8 flex items-center justify-center hover:bg-gray-100 focus:outline-none" @click="deleting = true" wire:click="delete">
+                    <x-heroicon-s-trash class="h-6 w-6 md:w-5 md:h-5 text-gray-500" />
                 </button>
             </div>
         @elseif ($type === 'checklist')
@@ -100,7 +100,7 @@
         @endif
     </div>
 
-    <div class="w-full mx-2">
+    <div class="w-full my-1 md:my-0 mx-2">
         <textarea
             wire:ignore
             x-ref="name"
