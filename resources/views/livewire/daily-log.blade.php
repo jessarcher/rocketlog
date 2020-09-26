460052777d0432633a274bd5bdb3c9e006ba025a
Jess Archer <jess@jessarcher.com>
2020-09-26 22:09:42 +1000

    Fix DOM diffing quirks

--- resources/views/livewire/daily-log.blade.php
+++ resources/views/livewire/daily-log.blade.php
@@ -4,16 +4,20 @@
         x-init="timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;"
     ></div>
     @foreach ($days as $day)
-        @php
-            $fade = $loop->iteration >= 5;
-        @endphp
+        <div wire:key="$day->date->timestamp">
+            @php
+                $fade = $loop->iteration >= 5;
+            @endphp
 
-        <h2 class="{{ $loop->first ? '' : 'mt-12' }} pb-3 font-bold border-b border-gray-200 {{ $fade ? 'text-gray-300' : 'text-gray-800' }}" wire:key="{{ $day->date }}">{{ $day->date->format('D, M d') }}</h2>
+            <h2 class="{{ $loop->first ? '' : 'mt-12' }} pb-3 font-bold border-b border-gray-200 {{ $fade ? 'text-gray-300' : 'text-gray-800' }}" wire:key="{{ $day->date }}">
+                {{ $day->date->format('D, M d') }}
+            </h2>
 
-        <div>
-            @foreach ($day->bullets as $bullet)
-                <livewire:bullet :bullet="$bullet" type="bullet" :fade="$fade" :key="$bullet->id" />
-            @endforeach
+            <div>
+                @foreach ($day->bullets as $bullet)
+                    <livewire:bullet :bullet="$bullet" type="bullet" :fade="$fade" :key="$bullet->id" />
+                @endforeach
+            </div>
         </div>
 
         @if ($loop->first)
