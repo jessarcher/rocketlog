8531a33fb412d1f1d446bc37809b4cffe6ba4863
Jess Archer <jess@jessarcher.com>
2020-09-30 22:29:30 +1000

    Make pusher scheme configurable

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,7 +40,7 @@ return [
                 'useTLS' => true,
                 'host' => '127.0.0.1',
                 'port' => 6001,
-                'scheme' => 'https',
+                'scheme' => env('PUSHER_APP_SCHEME', 'http'),
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
