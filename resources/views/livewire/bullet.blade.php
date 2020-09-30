56c87c86af735f1ea8dca8610705df3985220645
Jess Archer <jess@jessarcher.com>
2020-09-30 15:02:07 +1000

    Simplify loading UI behaviour

--- resources/views/livewire/bullet.blade.php
+++ resources/views/livewire/bullet.blade.php
@@ -2,38 +2,35 @@
     x-data="{
         menu: false,
         state: @entangle('bullet.state'),
-        deleting: false,
     }"
     class="py-1 md:py-2 border-b border-gray-200 flex"
     x-init="autosize($refs.name)"
 >
-    <div class="relative flex-shrink-0" :class="deleting ? 'opacity-25' : ''">
+    <div class="relative flex-shrink-0">
         @if ($type === 'bullet')
-            <div class="border border-transparent">
+            <div class="border border-transparent {{ $bullet->state === 'complete' || $fade ? 'opacity-50' : '' }}">
                 <button
-                    class="h-10 w-10 md:h-8 md:w-8 flex items-center justify-center rounded-full border border-transparent hover:border-gray-200 hover:shadow focus:outline-none focus:border-gray-200 focus:shadow-inner"
-                    :class="[
-                        ! $wire.fade && (state === 'incomplete' || state === 'note' || state === 'event') ? 'text-gray-700' : 'text-gray-400',
-                    ]"
+                    class="h-10 w-10 md:h-8 md:w-8 flex items-center justify-center rounded-full border border-transparent hover:border-gray-200 hover:shadow focus:outline-none focus:border-gray-200 focus:shadow-inner disabled:opacity-50"
                     @click="menu = true"
+                    wire:loading.attr="disabled"
                 >
                     @if ($bullet->state === 'incomplete')
-                        <x-icon.incomplete x-show="state === ''" class="h-6 w-6 md:h-5 md:h-5 text-gray-400" />
+                        <x-icon.incomplete x-show="state === ''" class="h-6 w-6 md:h-5 md:h-5" />
                     @endif
                     <x-icon.incomplete x-cloak x-show="state === 'incomplete'" class="h-6 w-6 md:h-5 md:h-5" />
 
                     @if ($bullet->state === 'complete')
-                        <x-icon.complete x-show="state === ''" class="h-6 w-6 md:h-5 md:h-5 text-gray-400" />
+                        <x-icon.complete x-show="state === ''" class="h-6 w-6 md:h-5 md:h-5" />
                     @endif
                     <x-icon.complete x-cloak x-show="state === 'complete'" class="h-6 w-6 md:h-5 md:h-5" />
 
                     @if ($bullet->state === 'note')
-                        <x-icon.note x-show="state === ''" class="h-6 w-6 md:h-5 md:h-5 text-gray-400" />
+                        <x-icon.note x-show="state === ''" class="h-6 w-6 md:h-5 md:h-5" />
                     @endif
                     <x-icon.note x-cloak x-show="state === 'note'" class="h-6 w-6 md:h-5 md:h-5" />
 
                     @if ($bullet->state === 'event')
-                        <x-icon.event x-show="state === ''" class="h-6 w-6 md:h-5 md:h-5 text-gray-400" />
+                        <x-icon.event x-show="state === ''" class="h-6 w-6 md:h-5 md:h-5" />
                     @endif
                     <x-icon.event x-cloak x-show="state === 'event'" class="h-6 w-6 md:h-5 md:h-5" />
                 </button>
@@ -77,7 +74,7 @@
                     </button>
                 @endif
 
-                <button class="h-10 w-10 md:h-8 md:w-8 flex items-center justify-center hover:bg-gray-100 focus:outline-none" @click="deleting = true" wire:click="delete">
+                <button class="h-10 w-10 md:h-8 md:w-8 flex items-center justify-center hover:bg-gray-100 focus:outline-none" wire:click="delete">
                     <x-heroicon-s-trash class="h-6 w-6 md:w-5 md:h-5 text-gray-500" />
                 </button>
             </div>
@@ -95,19 +92,19 @@
     </div>
 
     <div class="w-full mx-2">
-        <textarea
-            wire:ignore
-            x-ref="name"
-            wire:model.lazy="bullet.name"
-            wire:loading.attr="disabled"
-            class="w-full py-2 md:py-1 overflow-hidden bg-transparent disabled:opacity-50"
-            style="resize: none;"
-            rows="1"
-            :class="[
-                ! $wire.fade && (state === 'incomplete' || state === 'note' || state === 'event') ? 'text-gray-900' : 'text-gray-400',
-                deleting ? 'opacity-25' : ''
-            ]"
-        >{{ $bullet->name }}</textarea>
+        {{-- Apply the textarea opacity to a container so that it's applied regardless of the wire:ignore call --}}
+        <div class="{{ $bullet->state === 'complete' || $fade ? 'opacity-50' : '' }}">
+            <textarea
+                x-ref="name"
+                {{-- Prevent Livewire from re-rendering the textarea, which breaks the autosize --}}
+                wire:ignore
+                wire:model.lazy="bullet.name"
+                wire:loading.attr="disabled"
+                class="w-full py-2 md:py-1 overflow-hidden bg-transparent disabled:opacity-75"
+                style="resize: none;"
+                rows="1"
+            >{{ $bullet->name }}</textarea>
+        </div>
 
         <x-jet-input-error for="bullet.name" class="mt-2" />
         <x-jet-input-error for="bullet.state" class="mt-2" />
