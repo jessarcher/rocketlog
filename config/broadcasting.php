0d6c947cfd7f96e1624a2a8486c0f24ca7a64750
Jess Archer <jess@jessarcher.com>
2020-09-30 18:56:22 +1000

    Configure laravel-websockets as a Pusher replacement

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,6 +38,8 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
+                'host' => '127.0.0.1',
+                'port' => 6001
             ],
         ],
 
