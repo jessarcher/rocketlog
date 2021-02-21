b56a1f0c19191b41fc14d51f7d2cd9c89a8a3cf2
Jess Archer <jess@jessarcher.com>
2021-02-21 22:49:26 +1000

    Customise Jetstream buttons

--- resources/js/Jetstream/Button.vue
+++ resources/js/Jetstream/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
+    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gradient-to-br from-pink-500 to-purple-500 border border-transparent rounded-md font-bold text-sm text-white hover:from-purple-500 active:from-purple-500 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
         <slot></slot>
     </button>
 </template>
