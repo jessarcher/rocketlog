7672a98ee22e32b493e0d6311392b485e1239e7a
Jess Archer <jess@jessarcher.com>
2021-02-23 11:16:29 +1000

    Fix lists not being full-height on mobile

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -2,7 +2,7 @@
     <div>
         <jet-banner />
 
-        <div class="min-h-screen bg-gray-100">
+        <div class="flex flex-col min-h-screen bg-gray-100">
             <nav class="bg-white border-b border-gray-100">
                 <!-- Primary Navigation Menu -->
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -240,7 +240,7 @@
             <!-- </header> -->
 
             <!-- Page Content -->
-            <main>
+            <main class="flex-1 flex flex-col">
                 <slot></slot>
             </main>
 
