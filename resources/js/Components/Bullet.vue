7a4cf21edac347a71e164c6b0c7e3256ed064f62
Jess Archer <jess@jessarcher.com>
2021-02-23 10:38:46 +1000

    Add spinner when deleting a bullet

--- resources/js/Components/Bullet.vue
+++ resources/js/Components/Bullet.vue
@@ -6,7 +6,7 @@
                     <button
                         class="h-10 w-10 md:h-8 md:w-8 flex items-center justify-center rounded-full border border-transparent hover:border-gray-200 hover:shadow focus:outline-none focus:border-gray-200 focus:shadow-inner disabled:opacity-50"
                         @click="menu = true"
-                        :disabled="saving"
+                        :disabled="processing"
                     >
                         <incomplete-icon v-show="state === 'incomplete'" class="h-6 w-6 md:h-5" />
                         <complete-icon v-show="state === 'complete'" class="h-6 w-6 md:h-5" />
@@ -72,7 +72,7 @@
                 <textarea
                     ref="name"
                     v-model="name"
-                    :disabled="saving"
+                    :disabled="processing"
                     class="w-full p-2 md:p-1 overflow-hidden bg-transparent border-none disabled:opacity-75"
                     style="resize: none;"
                     rows="1"
@@ -88,7 +88,7 @@
         </div>
 
         <div class="w-10 h-10 md:w-8 md:h-8 border border-transparent flex items-center justify-center">
-            <svg v-if="saving" class="animate-spin h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <svg v-if="processing" class="animate-spin h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                 <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                 <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>
@@ -119,7 +119,7 @@ export default {
 
     data() {
         return {
-            saving: false,
+            processing: false,
             name: this.bullet.name,
             state: this.bullet.state,
             menu: false,
@@ -139,31 +139,35 @@ export default {
 
     watch: {
         async state(state) {
-            this.saving = true;
+            this.processing = true;
             await this.$listeners.input({ id: this.bullet.id, state })
-            this.saving = false;
+            this.processing = false;
         },
     },
 
     methods: {
         async save() {
-            this.saving = true;
+            this.processing = true;
 
             await this.$listeners.input({ id: this.bullet.id, name: this.name })
 
-            this.saving = false;
+            this.processing = false;
         },
 
         async migrate() {
-            this.saving = true;
+            this.processing = true;
 
             await this.$listeners.input({ id: this.bullet.id, date: this.date().startOf('day') })
 
-            this.saving = false;
+            this.processing = false;
         },
 
-        destroy() {
-            this.$emit('delete', this.bullet)
+        async destroy() {
+            this.processing = true;
+
+            await this.$listeners.delete(this.bullet)
+
+            this.processing = false;
         }
     },
 }
