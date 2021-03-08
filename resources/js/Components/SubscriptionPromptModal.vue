bd7df0b32edcaefbeb631d183bc705cb0e73259b
Jess Archer <jess@jessarcher.com>
2021-03-08 23:04:39 +1000

    Add extra padding to buttons

--- resources/js/Components/SubscriptionPromptModal.vue
+++ resources/js/Components/SubscriptionPromptModal.vue
@@ -40,7 +40,7 @@
                 Later
             </jet-secondary-button>
 
-            <a href="/billing" class="ml-2 inline-flex items-center px-4 py-2 bg-gradient-to-r from-pink-500 to-purple-700 rounded-md font-bold text-sm text-white hover:from-pink-600 hover:to-purple-800 active:from-pink-600 active:to-purple-800 focus:outline-none focus:shadow-outline-gray transition ease-in-out duration-150">
+            <a href="/billing" class="ml-2 inline-flex items-center px-5 py-2 bg-gradient-to-r from-pink-500 to-purple-700 rounded-md font-bold text-sm text-white hover:from-pink-600 hover:to-purple-800 active:from-pink-600 active:to-purple-800 focus:outline-none focus:shadow-outline-gray transition ease-in-out duration-150">
                 Subscribe
             </a>
         </template>
--- resources/js/Jetstream/Button.vue
+++ resources/js/Jetstream/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gradient-to-r from-pink-500 to-purple-700 rounded font-bold text-sm text-white hover:from-pink-600 hover:to-purple-800 active:from-pink-600 active:to-purple-800 focus:outline-none focus:from-pink-600 focus:to-purple-800 focus:shadow-outline-gray transition ease-in-out duration-150">
+    <button :type="type" class="inline-flex items-center px-5 py-2 bg-gradient-to-r from-pink-500 to-purple-700 rounded font-bold text-sm text-white hover:from-pink-600 hover:to-purple-800 active:from-pink-600 active:to-purple-800 focus:outline-none focus:from-pink-600 focus:to-purple-800 focus:shadow-outline-gray transition ease-in-out duration-150">
         <slot></slot>
     </button>
 </template>
