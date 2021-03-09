26c8951db5008d150254536c82d0d2a0b608e820
Jess Archer <jess@jessarcher.com>
2021-03-09 18:16:51 +1000

    Add description title tag

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>RocketLog | Digital bullet journal with daily log and task migration</title>
 
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
