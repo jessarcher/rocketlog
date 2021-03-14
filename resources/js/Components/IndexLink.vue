3d75e3be3ceb32d0963592aed76d541177e5df9a
Jess Archer <jess@jessarcher.com>
2021-03-14 22:09:45 +1000

    Index link color tweaks

--- resources/js/Components/IndexLink.vue
+++ resources/js/Components/IndexLink.vue
@@ -1,9 +1,10 @@
 <template>
     <inertia-link
         :href="href"
-        class="flex items-center py-3 border-b border-gray-200 dark:border-gray-700 font-medium text-gray-500 dark:text-gray-300 hover:text-gray-900 dark:hover:text-pink-500 focus:text-gray-900 dark:focus:text-pink-500"
+        class="flex items-center py-3 border-b border-gray-200 dark:border-gray-700 font-medium  hover:text-gray-900 dark:hover:text-white focus:text-gray-900 dark:focus:text-white"
+        :class="active ? 'text-gray-900 dark:text-white' : 'text-gray-500 dark:text-gray-400'"
     >
-        <Icon :name="icon" class="h-4 w-4" :class="active ? 'text-pink-500' : 'text-gray-500'" />
+        <Icon :name="icon" class="h-4 w-4" :class="active ? 'text-pink-600 dark:text-pink-500' : 'text-gray-500'" />
         <span class="ml-2"><slot /></span>
     </inertia-link>
 </template>
