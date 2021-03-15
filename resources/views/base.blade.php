5c9b0f3275e057a461a7b033640ed4f24cd62835
Jess Archer <jess@jessarcher.com>
2021-03-15 15:58:29 +1000

    Add twitter card and OG tags

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -16,6 +16,19 @@
         <meta name="msapplication-TileColor" content="#ffffff">
         <meta name="theme-color" content="#ffffff">
 
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:site" content="@rocketlog">
+        <meta name="twitter:creator" content="@jessarchercodes">
+        <meta name="twitter:title" content="RocketLog - Unburden your mind.">
+        <meta name="twitter:description" content="Is your todo list an unmanageable guilt-trip of things you thought were important but haven't done? We've been there too.">
+        <meta name="twitter:image" content="https://rocketlog.app/images/rocketlog-twitter-card.png">
+
+        <meta property="og:title" content="RocketLog - Unburden your mind." />
+        <meta property="og:url" content="https://rocketlog.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="Is your todo list an unmanageable guilt-trip of things you thought were important but haven't done? We've been there too." />
+        <meta property="og:image" content="https://rocketlog.app/images/rocketlog-twitter-card.png" />
+
         <!-- Fonts -->
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
