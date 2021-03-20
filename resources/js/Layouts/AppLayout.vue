8695dfc78736212f35f113b4724e6e2814f8a73b
Jess Archer <jess@jessarcher.com>
2021-03-20 11:19:42 +1000

    Make mobile nav more visually obvious

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -166,90 +166,91 @@
                 </div>
 
                 <!-- Responsive Navigation Menu -->
-                <div :class="{'block': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}" class="sm:hidden fixed inset-0 border-t border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 overflow-y-auto mt-16">
-                    <!-- <div class="pt-2 pb-3 space-y-1"> -->
-                    <!--     <jet-responsive-nav-link :href="route('daily-log.index')" :active="route().current('daily-log.index')"> -->
-                    <!--         Daily Log -->
-                    <!--     </jet-responsive-nav-link> -->
-                    <!-- </div> -->
-
-                    <!-- Responsive Settings Options -->
-                    <div class="pt-4 pb-1">
-                        <div class="flex items-center px-4">
-                            <div v-if="$page.props.jetstream.managesProfilePhotos" class="flex-shrink-0 mr-3" >
-                                <img class="h-10 w-10 rounded-full object-cover" :src="$page.props.user.profile_photo_url" :alt="$page.props.user.name" />
-                            </div>
-
-                            <div>
-                                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ $page.props.user.name }}</div>
-                                <div class="font-medium text-sm text-gray-500">{{ $page.props.user.email }}</div>
-                            </div>
-                        </div>
-
-                        <div class="mt-3 space-y-1">
-                            <jet-responsive-nav-link :href="route('profile.show')" :active="route().current('profile.show')">
-                                Profile
-                            </jet-responsive-nav-link>
-
-                            <jet-responsive-nav-link :href="route('spark.portal')" as="a">
-                                Billing
-                            </jet-responsive-nav-link>
-
-                            <jet-responsive-nav-link @click.native="showingSupportModal = true" as="button">
-                                Support
-                            </jet-responsive-nav-link>
-
-                            <jet-responsive-nav-link :href="route('api-tokens.index')" :active="route().current('api-tokens.index')" v-if="$page.props.jetstream.hasApiFeatures">
-                                API Tokens
-                            </jet-responsive-nav-link>
-
-                            <!-- Authentication -->
-                            <form method="post" action="/logout">
-                                <input type="hidden" name="_token" :value="$page.props.csrf_token" />
-                                <jet-responsive-nav-link as="button">
-                                    Logout
-                                </jet-responsive-nav-link>
-                            </form>
-
-                            <!-- Team Management -->
-                            <template v-if="$page.props.jetstream.hasTeamFeatures">
-                                <div class="border-t border-gray-200"></div>
-
-                                <div class="block px-4 py-2 text-xs text-gray-400">
-                                    Manage Team
+                <div :class="{'flex': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}" class="sm:hidden justify-end fixed inset-0 border-t border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 bg-opacity-75 mt-16">
+                    <div class="w-2/3 bg-white dark:bg-gray-800 border-l border-gray-200 dark:border-gray-700 overflow-y-auto shadow-lg">
+                        <!-- <div class="pt-2 pb-3 space-y-1"> -->
+                        <!--     <jet-responsive-nav-link :href="route('daily-log.index')" :active="route().current('daily-log.index')"> -->
+                        <!--         Daily Log -->
+                        <!--     </jet-responsive-nav-link> -->
+                        <!-- </div> -->
+                        <!-- Responsive Settings Options -->
+                        <div class="pt-4 pb-1">
+                            <div class="flex items-center px-4">
+                                <div v-if="$page.props.jetstream.managesProfilePhotos" class="flex-shrink-0 mr-3" >
+                                    <img class="h-10 w-10 rounded-full object-cover" :src="$page.props.user.profile_photo_url" :alt="$page.props.user.name" />
                                 </div>
 
-                                <!-- Team Settings -->
-                                <jet-responsive-nav-link :href="route('teams.show', $page.props.user.current_team)" :active="route().current('teams.show')">
-                                    Team Settings
-                                </jet-responsive-nav-link>
-
-                                <jet-responsive-nav-link :href="route('teams.create')" :active="route().current('teams.create')">
-                                    Create New Team
-                                </jet-responsive-nav-link>
-
-                                <div class="border-t border-gray-200"></div>
-
-                                <!-- Team Switcher -->
-                                <div class="block px-4 py-2 text-xs text-gray-400">
-                                    Switch Teams
+                                <div>
+                                    <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ $page.props.user.name }}</div>
+                                    <div class="font-medium text-sm text-gray-500">{{ $page.props.user.email }}</div>
                                 </div>
+                            </div>
 
-                                <template v-for="team in $page.props.user.all_teams">
-                                    <form @submit.prevent="switchToTeam(team)" :key="team.id">
-                                        <jet-responsive-nav-link as="button">
-                                            <div class="flex items-center">
-                                                <svg v-if="team.id == $page.props.user.current_team_id" class="mr-2 h-5 w-5 text-green-400" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                                <div>{{ team.name }}</div>
-                                            </div>
-                                        </jet-responsive-nav-link>
-                                    </form>
+                            <div class="mt-3 space-y-1">
+                                <jet-responsive-nav-link :href="route('profile.show')" :active="route().current('profile.show')">
+                                    Profile
+                                </jet-responsive-nav-link>
+
+                                <jet-responsive-nav-link :href="route('spark.portal')" as="a">
+                                    Billing
+                                </jet-responsive-nav-link>
+
+                                <jet-responsive-nav-link @click.native="showingSupportModal = true" as="button">
+                                    Support &amp; Requests
+                                </jet-responsive-nav-link>
+
+                                <jet-responsive-nav-link :href="route('api-tokens.index')" :active="route().current('api-tokens.index')" v-if="$page.props.jetstream.hasApiFeatures">
+                                    API Tokens
+                                </jet-responsive-nav-link>
+
+                                <!-- Authentication -->
+                                <form method="post" action="/logout">
+                                    <input type="hidden" name="_token" :value="$page.props.csrf_token" />
+                                    <jet-responsive-nav-link as="button">
+                                        Logout
+                                    </jet-responsive-nav-link>
+                                </form>
+
+                                <!-- Team Management -->
+                                <template v-if="$page.props.jetstream.hasTeamFeatures">
+                                    <div class="border-t border-gray-200"></div>
+
+                                    <div class="block px-4 py-2 text-xs text-gray-400">
+                                        Manage Team
+                                    </div>
+
+                                    <!-- Team Settings -->
+                                    <jet-responsive-nav-link :href="route('teams.show', $page.props.user.current_team)" :active="route().current('teams.show')">
+                                        Team Settings
+                                    </jet-responsive-nav-link>
+
+                                    <jet-responsive-nav-link :href="route('teams.create')" :active="route().current('teams.create')">
+                                        Create New Team
+                                    </jet-responsive-nav-link>
+
+                                    <div class="border-t border-gray-200"></div>
+
+                                    <!-- Team Switcher -->
+                                    <div class="block px-4 py-2 text-xs text-gray-400">
+                                        Switch Teams
+                                    </div>
+
+                                    <template v-for="team in $page.props.user.all_teams">
+                                        <form @submit.prevent="switchToTeam(team)" :key="team.id">
+                                            <jet-responsive-nav-link as="button">
+                                                <div class="flex items-center">
+                                                    <svg v-if="team.id == $page.props.user.current_team_id" class="mr-2 h-5 w-5 text-green-400" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                                    <div>{{ team.name }}</div>
+                                                </div>
+                                            </jet-responsive-nav-link>
+                                        </form>
+                                    </template>
                                 </template>
-                            </template>
-                        </div>
+                            </div>
 
-                        <div class="mt-3 border-t border-gray-200 dark:border-gray-700 p-4">
-                            <index />
+                            <div class="mt-3 border-t border-gray-200 dark:border-gray-700 p-4">
+                                <index />
+                            </div>
                         </div>
                     </div>
                 </div>
