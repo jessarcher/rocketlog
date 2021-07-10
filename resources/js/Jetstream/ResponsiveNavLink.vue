6739c15d05b7f702835b974afff9a060253277bd
Jess Archer <jess@jessarcher.com>
2021-07-10 19:26:10 +1000

    Tweak responsive nav styles

--- resources/js/Jetstream/ResponsiveNavLink.vue
+++ resources/js/Jetstream/ResponsiveNavLink.vue
@@ -20,9 +20,10 @@
 
         computed: {
             classes() {
-                return this.active
-                            ? 'block pl-3 pr-4 py-2 border-l-4 border-pink-600 text-base font-medium text-gray-700 dark:text-gray-300 bg-pink-50 dark:bg-gray-700 focus:outline-none focus:text-pink-800 focus:bg-pink-100 focus:border-pink-700 transition duration-150 ease-in-out'
-                            : 'block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-800 dark:focus:text-gray-200 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out'
+                return 'block pl-3 pr-4 py-2 border-l-4 text-base font-medium transition duration-150 ease-in-out focus:outline-none '
+                    + (this.active
+                        ? 'bg-gray-50 dark:bg-gray-700 border-pink-600 text-gray-700 dark:text-gray-300 focus:text-pink-800 focus:bg-pink-100 focus:border-pink-700'
+                        : 'border-transparent text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 focus:text-gray-800 dark:focus:text-gray-200 focus:bg-gray-50 dark:focus:bg-gray-700 focus:border-gray-300')
             }
         }
     }
