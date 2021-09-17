ea88d2e6f7f5b23997ec90407853fcaf08c183cf
Jess Archer <jess@jessarcher.com>
2021-09-17 15:08:01 +1000

    Update tests

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Events\CollectionUpdated;
+use App\Events\DailyLogUpdated;
 use App\Models\Bullet;
 use App\Models\Collection;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 use Vinkla\Hashids\Facades\Hashids;
 
@@ -13,18 +16,21 @@ class CollectionTest extends TestCase
 
     public function test_it_requires_authentication()
     {
-        $this
-            ->post('/c')
-            ->assertRedirect('/login');
+        $response = $this->post('/c');
+
+        $response->assertRedirect('/login');
     }
 
     public function test_it_stores_a_new_collection()
     {
-        $this
-            ->actingAs($user = User::factory()->create())
-            ->post('/c', ['name' => 'Test collection'])
-            ->assertRedirect('/c/'.Hashids::encode(1));
+        /** @var \App\Models\User */
+        $user = User::factory()->create();
 
+        $response = $this
+            ->actingAs($user)
+            ->post('/c', ['name' => 'Test collection']);
+
+        $response->assertRedirect('/c/'.Hashids::encode(1));
         $this->assertDatabaseHas('collections', [
             'user_id' => $user->id,
             'name' => 'Test collection',
@@ -34,14 +40,18 @@ class CollectionTest extends TestCase
 
     public function test_it_shows_a_users_collection()
     {
-        $this->actingAs($user = User::factory()->create());
+        /** @var \App\Models\User */
+        $user = User::factory()->create();
         $collection = Collection::factory()->create(['user_id' => $user->id]);
         $bullet = Bullet::factory()->create(['collection_id' => $collection->id]);
         $otherUser = User::factory()->create();
         $collection->users()->attach($otherUser);
 
-        $this
-            ->get("/c/{$collection->hashid}")
+        $response = $this
+            ->actingAs($user)
+            ->get("/c/{$collection->hashid}");
+
+        $response
             ->assertOk()
             ->assertViewHas('page.props.collection.name', $collection->name)
             ->assertViewHas('page.props.collection.bullets.0.name', $bullet->name)
@@ -50,69 +60,89 @@ class CollectionTest extends TestCase
 
     public function test_it_shows_a_collection_that_a_user_has_access_to()
     {
-        $this->actingAs($user = User::factory()->create());
+        /** @var \App\Models\User */
+        $user = User::factory()->create();
         $collection = Collection::factory()->create();
         $collection->users()->attach($user);
 
-        $this
-            ->get("/c/{$collection->hashid}")
-            ->assertOk();
+        $response = $this
+            ->actingAs($user)
+            ->get("/c/{$collection->hashid}");
+
+        $response->assertOk();
     }
 
     public function test_it_forbids_collections_a_user_doesnt_have_access_to()
     {
+        /** @var \App\Models\User */
+        $user = User::factory()->create();
         $collection = Collection::factory()->create();
 
-        $this
-            ->actingAs(User::factory()->create())
-            ->get("/c/{$collection->hashid}")
-            ->assertForbidden();
+        $response = $this
+            ->actingAs($user)
+            ->get("/c/{$collection->hashid}");
+
+        $response->assertForbidden();
     }
 
     public function test_it_updates_a_collection()
     {
-        $this->actingAs($user = User::factory()->create());
+        Event::fake();
+        /** @var \App\Models\User */
+        $user = User::factory()->create();
         $collection = Collection::factory()->create(['user_id' => $user->id]);
 
-        $this
+        $response = $this
+            ->actingAs($user)
             ->patch("/c/{$collection->hashid}", [
                 'name' => 'New collection name',
                 'type' => 'checklist',
                 'hide_done' => true,
-            ])
-            ->assertRedirect("/c/{$collection->hashid}");
+            ]);
 
+        $response->assertRedirect("/c/{$collection->hashid}");
         $this->assertDatabaseHas('collections', [
             'id' => $collection->id,
             'name' => 'New collection name',
             'type' => 'checklist',
             'hide_done' => true,
         ]);
+        Event::assertDispatched(fn (CollectionUpdated $event) => $event->collectionId === $collection->id);
     }
 
     public function test_it_destroys_a_collection()
     {
-        $this->actingAs($user = User::factory()->create());
+        Event::fake();
+        /** @var \App\Models\User */
+        $user = User::factory()->create();
         $collection = Collection::factory()->create(['user_id' => $user->id]);
 
-        $this
-            ->delete("/c/{$collection->hashid}")
-            ->assertRedirect('/daily-log');
+        $response = $this
+            ->actingAs($user)
+            ->delete("/c/{$collection->hashid}");
+
+        $response->assertRedirect('/daily-log');
+        Event::assertDispatched(fn (CollectionUpdated $event) => $event->collectionId === $collection->id);
     }
 
     public function test_only_the_owner_can_delete_the_collection()
     {
-        $this->actingAs($user = User::factory()->create());
+        /** @var \App\Models\User */
+        $user = User::factory()->create();
         $collection = Collection::factory()->create();
         $collection->users()->attach($user);
 
-        $this
-            ->delete("/c/{$collection->hashid}")
-            ->assertForbidden();
+        $response = $this
+            ->actingAs($user)
+            ->delete("/c/{$collection->hashid}");
+
+        $response->assertForbidden();
     }
 
     public function test_users_can_add_bullets_to_the_daily_log()
     {
+        Event::fake();
+        /** @var \App\Models\User */
         $user = User::factory()->create();
         $collection = $user->collections()->save(Collection::factory()->make());
         $bullet = $collection->bullets()->save(
@@ -130,10 +160,14 @@ class CollectionTest extends TestCase
 
         $response->assertRedirect("/c/{$collection->hashid}");
         $this->assertEquals('2021-02-03', $bullet->fresh()->date->format('Y-m-d'));
+        Event::assertDispatched(fn (CollectionUpdated $event) => $event->collectionId === $collection->id);
+        Event::assertDispatched(fn (DailyLogUpdated $event) => $event->userId === $user->id);
     }
 
     public function test_other_users_cant_add_bullets_to_the_owners_daily_log()
     {
+        Event::fake();
+        /** @var \App\Models\User */
         $user = User::factory()->create();
         $collection = Collection::factory()->create();
         $collection->users()->attach($user);
@@ -150,10 +184,13 @@ class CollectionTest extends TestCase
 
         $response->assertRedirect("/c/{$collection->hashid}");
         $this->assertNull($bullet->fresh()->date);
+        Event::assertDispatched(fn (CollectionUpdated $event) => $event->collectionId === $collection->id);
+        Event::assertNotDispatched(DailyLogUpdated::class);
     }
 
     public function test_only_an_authorized_user_can_invite_someone_to_a_collection()
     {
+        /** @var \App\Models\User */
         $user = User::factory()->create();
         $collection = Collection::factory()->create();
         $friend = User::factory()->create();
@@ -169,6 +206,7 @@ class CollectionTest extends TestCase
 
     public function test_users_can_be_invited_to_a_collection()
     {
+        /** @var \App\Models\User */
         $user = User::factory()->create();
         $collection = $user->collections()->save(Collection::factory()->make());
         $friend = User::factory()->create();
@@ -180,12 +218,12 @@ class CollectionTest extends TestCase
             ]);
 
         $response->assertRedirect("/c/{$collection->hashid}");
-
         $this->assertTrue($collection->users->contains($friend));
     }
 
     public function test_a_user_must_exist_to_be_invited_to_a_collection()
     {
+        /** @var \App\Models\User */
         $user = User::factory()->create();
         $collection = $user->collections()->save(Collection::factory()->make());
 
@@ -200,6 +238,7 @@ class CollectionTest extends TestCase
 
     public function test_a_user_cannot_be_invited_to_a_collection_twice()
     {
+        /** @var \App\Models\User */
         $user = User::factory()->create();
         $collection = $user->collections()->save(Collection::factory()->make());
         $friend = User::factory()->create();
