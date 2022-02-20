a0850686af14722012cab826811cae80ec3842eb
Shift <shift@laravelshift.com>
2022-02-20 10:58:20 +0000

    Adopt anonymous migrations

--- database/migrations/0000_00_00_000000_rename_statistics_counters.php
+++ database/migrations/0000_00_00_000000_rename_statistics_counters.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RenameStatisticsCounters extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -33,4 +33,4 @@ class RenameStatisticsCounters extends Migration
             $table->renameColumn('api_messages_count', 'api_message_count');
         });
     }
-}
+};
