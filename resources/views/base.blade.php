28c1b552ec3709936b088097b5387adce64e1da7
Jess Archer <jess@jessarcher.com>
2022-03-05 22:01:38 +1000

    Fix annoying pinch zoom on mobile

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>RocketLog | Digital bullet journal with daily log and task migration</title>
