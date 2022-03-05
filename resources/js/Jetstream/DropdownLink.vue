9f531b23e0acf96299c5393c6077fa96acd3b48a
Jess Archer <jess@jessarcher.com>
2022-03-05 21:45:51 +1000

    Increase text size of dropdown menu links

--- resources/js/Jetstream/DropdownLink.vue
+++ resources/js/Jetstream/DropdownLink.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
-        <button type="submit" class="block w-full px-4 py-2 text-sm leading-5 text-gray-700 dark:text-gray-100 text-left hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out" v-if="as == 'button'">
+        <button type="submit" class="block w-full px-4 py-2 text-base leading-6 text-gray-700 dark:text-gray-100 text-left hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out" v-if="as == 'button'">
             <slot></slot>
         </button>
 
-        <a :href="href" class="block px-4 py-2 text-sm leading-5 text-gray-700 dark:text-gray-100 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out" v-else-if="as == 'a'">
+        <a :href="href" class="block px-4 py-2 text-base leading-6 text-gray-700 dark:text-gray-100 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out" v-else-if="as == 'a'">
             <slot></slot>
         </a>
 
-        <Link :href="href" class="block px-4 py-2 text-sm leading-5 text-gray-700 dark:text-gray-100 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out" v-else>
+        <Link :href="href" class="block px-4 py-2 text-base leading-6 text-gray-700 dark:text-gray-100 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out" v-else>
             <slot></slot>
         </Link>
     </div>
