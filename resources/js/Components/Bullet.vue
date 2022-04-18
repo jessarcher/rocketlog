315f8587ba4e311ccaeaa6e786a164b41b4f3063
Jess Archer <jess@jessarcher.com>
2022-04-19 09:47:14 +1000

    Improve drag handle

--- resources/js/Components/Bullet.vue
+++ resources/js/Components/Bullet.vue
@@ -169,8 +169,8 @@
                 <div class="flex items-center">
                     <slot name="status" />
 
-                    <button v-if="draggable" class="p-2 my-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="drag-handle h-4 w-4 text-gray-400 dark:text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <button v-if="draggable" class="drag-handle p-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400 dark:text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16" />
                         </svg>
                     </button>
