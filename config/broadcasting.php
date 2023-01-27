459d848ffb1c2f675686d22b355aab02d25ef561
Shift <shift@laravelshift.com>
2023-01-27 01:00:20 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them and merged your true customizations -
    where ENV variables may not be used.

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,9 +37,12 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => false, // This will be handled by nginx
-                'host' => env('PUSHER_APP_HOST', '127.0.0.1'),
-                'port' => 6001,
+
+                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
+                'port' => env('PUSHER_PORT', 443),
+                'scheme' => env('PUSHER_SCHEME', 'https'),
+                'encrypted' => true,
+                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
