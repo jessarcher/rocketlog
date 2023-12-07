755b972c1fcb0de90785555ccd1b41ce982dffd1
Jess Archer <jess@jessarcher.com>
2023-12-07 19:05:35 +1000

    Misc Shift tweaks

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,12 +37,9 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
-                'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                'useTLS' => false, // This will be handled by nginx
+                'host' => env('PUSHER_APP_HOST', '127.0.0.1'),
+                'port' => 6001,
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
